53131865ef292a9273ee2bfe37a92a4e8c9a9f20
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-03-05 17:56:47 +0330

    chore: remove docker strapi/ backend

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd55f29e9e8f7f6d2cc734498064ec104'),
-    },
-  },
-});
