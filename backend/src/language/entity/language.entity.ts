fef51b27ff0e96238c5a5c4c7f7c5449fc6fe74f
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-03-12 22:17:52 +0330

    feat: Add language entity

---
+++ backend/src/language/entity/language.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, EntityBuilder, Record } from 'src/utilities/entity'
+
+export class Language implements Entity {
+  constructor(private readonly id: string, private readonly local: string) {}
+}
+
+interface languageEntities {
+  id: string
+  local: string
+}
+
+export class LanguageEntityBuilder implements EntityBuilder {
+  static create(record: Record): Language {
+    const language = record.get('language')
+    // TODO: cast type `as Record<languageEntities>`
+    const { id, local } = language.properties
+    return new language(id, local)
+  }
+}
--- backend/src/main.module.ts
+++ backend/src/main.module.ts
@@ -4,12 +4,14 @@ import { NEO4J_OPTIONS } from './main-configs'
 // modules
 import { WordModule } from './word/word.module'
 import { CategoryModule } from './category/category.module'
+import { LanguageModule } from './language/language.module'
 
 @Module({
   imports: [
     Neo4jModule.forRoot(NEO4J_OPTIONS as Neo4jConfig),
     WordModule,
     CategoryModule,
+    LanguageModule,
   ],
   controllers: [],
   providers: [],
