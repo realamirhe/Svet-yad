85fb55fe77a09be47c8359e840ddfa5f2bf4c32b
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-03-12 22:30:46 +0330

    feat: convenstion about word builder

--- backend/src/word/entity/word.entity.ts
+++ backend/src/word/entity/word.entity.ts
@@ -4,9 +4,15 @@ export class Word implements Entity {
   constructor(private readonly name: string) {}
 }
 
+interface wordEntities {
+  name: string
+}
+
 export class WordEntityBuilder implements EntityBuilder {
   static create(record: Record): Word {
-    const name = record.get('name')
+    const word = record.get('word')
+    // TODO: cast type `as Record<wordEntities>`
+    const { name } = word.properties
     return new Word(name)
   }
 }
