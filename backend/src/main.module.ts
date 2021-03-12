11abfc4827d4e4ecb49d11b6cca92292b5153702
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-03-12 22:15:18 +0330

    feat: add category entity

--- backend/src/main.module.ts
+++ backend/src/main.module.ts
@@ -3,9 +3,14 @@ import { Neo4jModule, Neo4jConfig } from 'nest-neo4j'
 import { NEO4J_OPTIONS } from './main-configs'
 // modules
 import { WordModule } from './word/word.module'
+import { CategoryModule } from './category/category.module'
 
 @Module({
-  imports: [Neo4jModule.forRoot(NEO4J_OPTIONS as Neo4jConfig), WordModule],
+  imports: [
+    Neo4jModule.forRoot(NEO4J_OPTIONS as Neo4jConfig),
+    WordModule,
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
